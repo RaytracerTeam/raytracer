1c4ccefb837c221c94361872818937fe9b51b4df
Arcod7 <antoine.esman@epitech.eu>
2024-04-22 12:41:54 +0200

    added : libconfig++ first steps

---
+++ scenes/test.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** test
+*/
+
+#include <iostream>
+#include <libconfig.h++>
+
+int main(void)
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    libconfig::Config cfg;
+    cfg.readFile("./demo_spheres.cfg");
+    auto &cameras = cfg.lookup("cameras");
+    float fov;
+    int width;
+    for (auto &camera : cameras) {
+        camera.lookup("resolution").lookupValue("width", width);
+        camera.lookupValue("fieldOfView", fov);
+        std::cout << fov << std::endl;
+        std::cout << width << std::endl;
+    }
+
+    return 0;
+}
