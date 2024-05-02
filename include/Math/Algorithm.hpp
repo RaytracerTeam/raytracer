41e4c6f82eeb3fda15db7cddb1559596a4b5b0e6
Nathan FLATTIN <nathan.flattin@epitech.eu>
2024-05-03 01:00:47 +0200

    feat: algorithm class with sort, cubic solver and quartic solver

--- include/Math/Algorithm.hpp
+++ include/Math/Algorithm.hpp
@@ -6,12 +6,61 @@
 */
 
 #pragma once
-namespace Raytracer
-{
+
+#include <array>
+#include <cmath>
+#include <complex>
+
+#define TOLERANCE 1.0e-8
+
+namespace Raytracer {
     namespace Math {
+        typedef std::complex<double> complex;
+
         class Algorithm {
-            public:
-                static double clampD(double value, double min, double max);
+        public:
+            static double clampD(double value, double min, double max);
+
+            static inline bool isZero(complex x)
+            {
+                return (fabs(x.real()) < TOLERANCE)
+                    && (fabs(x.imag()) < TOLERANCE);
+            }
+
+            static void sortRoots(double *roots, int n);
+
+            static int filterRealNumbers(int numComplexValues,
+                const complex inArray[], double outArray[]);
+
+            static complex cbrt(complex a, int n);
+
+            static int solveCubicEquation(
+                complex a, complex b, complex c, complex d, complex roots[3]);
+
+            static inline int solveCubicEquation(
+                double a, double b, double c, double d, double roots[3])
+            {
+                complex croots[3];
+
+                const int numComplexRoots = solveCubicEquation(
+                    complex(a), complex(b), complex(c), complex(d), croots);
+
+                return filterRealNumbers(numComplexRoots, croots, roots);
+            }
+
+            static int solveQuarticEquation(complex a, complex b, complex c,
+                complex d, complex e, complex roots[4]);
+
+            static inline int solveQuarticEquation(double a, double b, double c,
+                double d, double e, double roots[4])
+            {
+                complex croots[4];
+
+                const int numComplexRoots = solveQuarticEquation(complex(a),
+                    complex(b), complex(c), complex(d), complex(e), croots);
+
+                return filterRealNumbers(numComplexRoots, croots, roots);
+            }
         };
     }
 } // namespace Raytracer
