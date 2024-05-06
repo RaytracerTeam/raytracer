495fe6e54924f018c6ace8e13490baf4862492f0
Nathan FLATTIN <nathan.flattin@epitech.eu>
2024-05-06 14:29:39 +0200

    fix: cylinder getNormal

--- src/scene/primitives/Cylinder.cpp
+++ src/scene/primitives/Cylinder.cpp
@@ -17,7 +17,7 @@ namespace Raytracer {
             Math::Vector3D(m_origin.getX() + m_radius, m_origin.getY() + m_height, m_origin.getZ() + m_radius));
     }
 
-    static RayHit getNormal(double distance, const Math::Vector3D &hitPt, const Math::Vector3D &origin)
+    RayHit Cylinder::getNormal(double distance, const Math::Vector3D &hitPt, const Math::Vector3D &origin) const
     {
         auto normal = Math::Vector3D(
             hitPt.getX() - origin.getX(), 0, hitPt.getZ() - origin.getZ())
