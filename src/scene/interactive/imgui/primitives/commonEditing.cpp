553e7f04a7bfdaae7a5c0af9bc61da9d9ea895eb
Arcod7 <antoine.esman@epitech.eu>
2024-05-09 19:33:10 +0200

    fixed : compilation error

--- src/scene/interactive/imgui/primitives/commonEditing.cpp
+++ src/scene/interactive/imgui/primitives/commonEditing.cpp
@@ -11,8 +11,11 @@ namespace Raytracer
 {
     void SceneInteractive::guiColoredSquare(const Color &color)
     {
+        (void)color;
+        #ifdef BONUS
         ImGui::ColorButton(" ", ImVec4(color.getR(),
             color.getG(), color.getB(), 1.0f),
             ImGuiColorEditFlags_InputRGB);
+        #endif
     }
 } // namespace Raytracer
